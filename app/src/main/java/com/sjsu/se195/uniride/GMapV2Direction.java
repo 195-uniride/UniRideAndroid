77beef01b5ed7b0e3bdccdf16728e8f0a027296a
maninderjits <sukhisembhy@hotmail.com>
2018-02-11 20:05:13 -0800

    Should see source and destination and their route on map when adding them

---
+++ app/src/main/java/com/sjsu/se195/uniride/GMapV2Direction.java
@@ -0,0 +1,206 @@
+package com.sjsu.se195.uniride;
+
+/**
+ * Added by maninder on 2/8/18.
+ */
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+/*import org.apache.http.HttpURLConnection;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;*/
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class GMapV2Direction  extends AsyncTask<LatLng, Void, Document>{
+    public final static String MODE_DRIVING = "driving";
+    public final static String MODE_WALKING = "walking";
+    public Document document;
+    public GMapV2Direction() { }
+
+    @Override
+    protected Document doInBackground(LatLng... latLngs) {
+        document = null;
+        String url_string = "http://maps.googleapis.com/maps/api/directions/xml?"
+                + "origin=" + latLngs[0].latitude + "," + latLngs[0].longitude
+                + "&destination=" + latLngs[1].latitude + "," + latLngs[1].longitude
+                + "&sensor=false&units=metric&mode=driving";
+        URL url;
+        HttpURLConnection urlConnection = null;
+
+        try {
+            url = new URL(url_string);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoInput(true);
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            document = builder.parse(in);
+
+        } catch (Exception e) {
+            System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+            e.printStackTrace();
+        }
+        //Thread.sleep(500000);tions class");
+        return document;
+    }
+
+    public Document getDocument(LatLng start, LatLng end, String mode) {
+        System.out.println("first is getDocument");
+        Document doc = null;
+        System.out.println("GMapV2 get direct");
+        return doc;
+    }
+
+    public String getDurationText (Document doc) {
+        NodeList nl1 = doc.getElementsByTagName("duration");
+        Node node1 = nl1.item(0);
+        NodeList nl2 = node1.getChildNodes();
+        Node node2 = nl2.item(getNodeIndex(nl2, "text"));
+        Log.i("DurationText", node2.getTextContent());
+        return node2.getTextContent();
+    }
+
+    public int getDurationValue (Document doc) {
+        NodeList nl1 = doc.getElementsByTagName("duration");
+        Node node1 = nl1.item(0);
+        NodeList nl2 = node1.getChildNodes();
+        Node node2 = nl2.item(getNodeIndex(nl2, "value"));
+        Log.i("DurationValue", node2.getTextContent());
+        return Integer.parseInt(node2.getTextContent());
+    }
+
+    public String getDistanceText (Document doc) {
+        NodeList nl1 = doc.getElementsByTagName("distance");
+        Node node1 = nl1.item(0);
+        NodeList nl2 = node1.getChildNodes();
+        Node node2 = nl2.item(getNodeIndex(nl2, "text"));
+        Log.i("DistanceText", node2.getTextContent());
+        return node2.getTextContent();
+    }
+
+    public int getDistanceValue (Document doc) {
+        NodeList nl1 = doc.getElementsByTagName("distance");
+        Node node1 = nl1.item(0);
+        NodeList nl2 = node1.getChildNodes();
+        Node node2 = nl2.item(getNodeIndex(nl2, "value"));
+        Log.i("DistanceValue", node2.getTextContent());
+        return Integer.parseInt(node2.getTextContent());
+    }
+
+    public String getStartAddress (Document doc) {
+        NodeList nl1 = doc.getElementsByTagName("start_address");
+        Node node1 = nl1.item(0);
+        Log.i("StartAddress", node1.getTextContent());
+        return node1.getTextContent();
+    }
+
+    public String getEndAddress (Document doc) {
+        NodeList nl1 = doc.getElementsByTagName("end_address");
+        Node node1 = nl1.item(0);
+        Log.i("StartAddress", node1.getTextContent());
+        return node1.getTextContent();
+    }
+
+    public String getCopyRights (Document doc) {
+        NodeList nl1 = doc.getElementsByTagName("copyrights");
+        Node node1 = nl1.item(0);
+        Log.i("CopyRights", node1.getTextContent());
+        return node1.getTextContent();
+    }
+
+    public ArrayList<LatLng> getDirection (Document doc) {
+        NodeList nl1, nl2, nl3;
+        ArrayList<LatLng> listGeopoints = new ArrayList<LatLng>();
+        nl1 = doc.getElementsByTagName("step");
+        if (nl1.getLength() > 0) {
+            for (int i = 0; i < nl1.getLength(); i++) {
+                Node node1 = nl1.item(i);
+                nl2 = node1.getChildNodes();
+
+                Node locationNode = nl2.item(getNodeIndex(nl2, "start_location"));
+                nl3 = locationNode.getChildNodes();
+                Node latNode = nl3.item(getNodeIndex(nl3, "lat"));
+                double lat = Double.parseDouble(latNode.getTextContent());
+                Node lngNode = nl3.item(getNodeIndex(nl3, "lng"));
+                double lng = Double.parseDouble(lngNode.getTextContent());
+                listGeopoints.add(new LatLng(lat, lng));
+
+                locationNode = nl2.item(getNodeIndex(nl2, "polyline"));
+                nl3 = locationNode.getChildNodes();
+                latNode = nl3.item(getNodeIndex(nl3, "points"));
+                ArrayList<LatLng> arr = decodePoly(latNode.getTextContent());
+                for(int j = 0 ; j < arr.size() ; j++) {
+                    listGeopoints.add(new LatLng(arr.get(j).latitude, arr.get(j).longitude));
+                }
+
+                locationNode = nl2.item(getNodeIndex(nl2, "end_location"));
+                nl3 = locationNode.getChildNodes();
+                latNode = nl3.item(getNodeIndex(nl3, "lat"));
+                lat = Double.parseDouble(latNode.getTextContent());
+                lngNode = nl3.item(getNodeIndex(nl3, "lng"));
+                lng = Double.parseDouble(lngNode.getTextContent());
+                listGeopoints.add(new LatLng(lat, lng));
+            }
+        }
+
+        return listGeopoints;
+    }
+
+    private int getNodeIndex(NodeList nl, String nodename) {
+        for(int i = 0 ; i < nl.getLength() ; i++) {
+            if(nl.item(i).getNodeName().equals(nodename))
+                return i;
+        }
+        return -1;
+    }
+
+    private ArrayList<LatLng> decodePoly(String encoded) {
+        ArrayList<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng position = new LatLng((double) lat / 1E5, (double) lng / 1E5);
+            poly.add(position);
+        }
+        return poly;
+    }
+
+
+}
