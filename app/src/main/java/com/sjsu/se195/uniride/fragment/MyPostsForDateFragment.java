a192bc60fe5168ed7a71d56996e8c063333c0720
maninderjits <sukhisembhy@hotmail.com>
2018-03-11 17:00:38 -0700

    Created carpoolActivity and fragment

---
+++ app/src/main/java/com/sjsu/se195/uniride/fragment/MyPostsForDateFragment.java
@@ -0,0 +1,32 @@
+package com.sjsu.se195.uniride.fragment;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class MyPostsForDateFragment extends PostListFragment {
+
+    public MyPostsForDateFragment() {}
+
+    @Override
+    public Query getQuery(DatabaseReference databaseReference) {
+        // [START recent_posts_query]
+        // Last 100 posts, these are automatically the 100 most recent
+        // due to sorting by push() keys
+        Query recentPostsQuery;
+
+        // Load Drive Offer Posts:
+        if(!postType){
+            recentPostsQuery = getAllDriveOfferPosts().limitToFirst(100);
+        }
+        // Load Ride Request Posts:
+        else{
+            recentPostsQuery = getAllRideRequestPosts().limitToFirst(100);
+        }
+        // [END recent_posts_query]
+
+        return recentPostsQuery;
+    }
+}
